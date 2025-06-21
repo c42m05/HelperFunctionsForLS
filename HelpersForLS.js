50d1b7f0d6442923d5b7a33ae9ce7e08b7d4227d
Can <87504696+c42m05@users.noreply.github.com>
2025-06-21 17:49:29 +0300

    Update HelpersForLS.js

--- HelpersForLS.js
+++ HelpersForLS.js
@@ -1,5 +1,12 @@
-// Helper Utils for LS  © 2025 by c4205M(c42m05) is licensed under CC BY 4.0. To view a copy of this license, visit https://creativecommons.org/licenses/by/4.0/
+/**
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published 
+ * by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied 
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>. 
+ */
 
+// https://github.com/c42m05/HelperFunctionsForLS
 
 //#region <BATCH UTILS DEFINITIONS> 
 /**
